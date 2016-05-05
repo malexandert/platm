b325149df11c60db0ce772349b182209bb3815fe
Marcus Todd <malexander.todd@gmail.com>
2016-05-04 21:07:30 -0400

    Some additions to the tm abstract class

--- app/turing/TuringMachine.scala
+++ app/turing/TuringMachine.scala
@@ -1,9 +1,15 @@
-package turing.TuringUtil
+package turing
+
 
 abstract class TuringMachine(
     Q: Set[State],
     Sigma: Alphabet,
     q0: State,
     qH: State) {
+  require((Q contains q0) && (Q contains qH))
+
+  var tape: String = ""
+
+  def delta: Map[(State, Char), (State, Char, Direction)]
 
 }
--- app/turing/package.scala
+++ app/turing/package.scala
@@ -1,6 +1,10 @@
-package turing
 
-package object TuringUtil {
+package object turing {
+  sealed trait Direction
+  case object Left extends Direction
+  case object Right extends Direction
   type State = Int
+
   type Alphabet = Set[Char]
+  type Transition = ((State, Char), (State, Char, Direction))
 }
